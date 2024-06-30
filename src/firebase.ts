319667fc6957184b8f8b7c62d4782451dd837756
iberis2 <spark@blast-team.com>
2024-06-30 14:35:46 +0900

    chore: firebase 설치

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDYbcumIOnHifYBMwTsbOJPmWsySKwRX-4',
+  authDomain: 'fir-twitter-4b4d9.firebaseapp.com',
+  projectId: 'fir-twitter-4b4d9',
+  storageBucket: 'fir-twitter-4b4d9.appspot.com',
+  messagingSenderId: '794243458963',
+  appId: '1:794243458963:web:9c3fae522446c732b62a66',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

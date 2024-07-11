69c5aaf7507ef387e1ce4bb88bad7108088b1f6f
iberis2 <spark@blast-team.com>
2024-07-11 14:47:15 +0900

    feat: add login

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,45 +1,9 @@
 import { useState } from 'react';
-import styled from 'styled-components';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../firebase';
-import { useNavigate } from 'react-router-dom';
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-const Title = styled.h1`
-  font-size: 42px;
-`;
-const Form = styled.form`
-  margin-top: 50px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type='submit'] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
+import { Link, useNavigate } from 'react-router-dom';
+import { FirebaseError } from 'firebase/app';
+import { Error, Form, Input, Switcher, Title, Wrapper } from '../components/auth-components';
 
 export default function CreateAccount() {
   const navigate = useNavigate();
@@ -61,6 +25,7 @@ export default function CreateAccount() {
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setError('');
     if (isLoading) return;
     if (!inputValue.email || !inputValue.password || !inputValue.name) {
       setError('Please fill out all fields');
@@ -78,8 +43,9 @@ export default function CreateAccount() {
       await updateProfile(credential.user, { displayName: inputValue.name });
       navigate('/');
     } catch (error) {
-      console.error(error);
-      // setError(error);
+      if (error instanceof FirebaseError) {
+        setError(error.message);
+      }
     } finally {
       setIsLoading(false);
     }
@@ -120,6 +86,9 @@ export default function CreateAccount() {
         />
       </Form>
       {error && <Error>{error}</Error>}
+      <Switcher>
+        Already have an account? <Link to='/login'>Log in</Link>
+      </Switcher>
     </Wrapper>
   );
 }
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,3 +1,76 @@
+import { useState } from 'react';
+
+import { Link, useNavigate } from 'react-router-dom';
+import { FirebaseError } from 'firebase/app';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
+import { Error, Form, Input, Switcher, Title, Wrapper } from '../components/auth-components';
+
 export default function Login() {
-  return <h1>Login</h1>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [inputValue, setInputValue] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError('');
+    if (isLoading) return;
+    if (!inputValue.email || !inputValue.password) {
+      setError('Please fill out all fields');
+      return;
+    }
+    try {
+      setIsLoading(true);
+      // login
+      await signInWithEmailAndPassword(auth, inputValue.email, inputValue.password);
+      navigate('/');
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        setError(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Title>Log into 𝕏 </Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          name='email'
+          onChange={onChange}
+          value={inputValue.email}
+          type='email'
+          placeholder='Email'
+          required
+        />
+        <Input
+          name='password'
+          onChange={onChange}
+          value={inputValue.password}
+          type='password'
+          placeholder='Password'
+          required
+        />
+        <Input type='submit' value={isLoading ? 'Loading...' : 'Log in'} disabled={isLoading} />
+      </Form>
+      {error && <Error>{error}</Error>}
+      <Switcher>
+        Don't have an account? <Link to='/create-account'>Create one</Link>
+      </Switcher>
+    </Wrapper>
+  );
 }

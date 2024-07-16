cbce941f6a68824bed6a9c652a6596702b3f8fa8
iberis2 <spark@blast-team.com>
2024-07-16 17:18:13 +0900

    refactor: tweet list 의 변화에 따라 실시간으로 반영해서 보여주도록 onSnapShot 으로 함수 변경

--- src/components/time-line.tsx
+++ src/components/time-line.tsx
@@ -1,8 +1,9 @@
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { collection, getDocs, onSnapshot, orderBy, query, Unsubscribe } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { db } from '../firebase';
 import Tweet from './tweet';
+import { limit } from 'firebase/firestore';
 
 export interface ITweet {
   photo?: string;
@@ -20,18 +21,30 @@ const Wrapper = styled.div`
 
 export default function TimeLine() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const tweetsQuery = query(collection(db, 'tweets'), orderBy('createdAt', 'desc'));
-    const snapShot = await getDocs(tweetsQuery);
-    const tweets = snapShot.docs.map(doc => {
-      const { photo, tweet, username, createdAt, userId } = doc.data();
-      return { id: doc.id, photo, tweet, username, createdAt, userId };
-    });
-    setTweets(tweets);
-  };
 
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const tweetsQuery = query(collection(db, 'tweets'), orderBy('createdAt', 'desc'), limit(25));
+      // const snapShot = await getDocs(tweetsQuery);
+      // const tweets = snapShot.docs.map(doc => {
+      //   const { photo, tweet, username, createdAt, userId } = doc.data();
+      //   return { id: doc.id, photo, tweet, username, createdAt, userId };
+      // });
+      unsubscribe = await onSnapshot(tweetsQuery, snapShot => {
+        const tweets = snapShot.docs.map(doc => {
+          const { photo, tweet, username, createdAt, userId } = doc.data();
+          return { id: doc.id, photo, tweet, username, createdAt, userId };
+        });
+        setTweets(tweets);
+      });
+    };
     fetchTweets();
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
   }, []);
   return (
     <Wrapper>
